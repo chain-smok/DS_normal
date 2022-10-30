32206da60374a0b8db23d86efb316ccb75d6eb57
chain-smok <s1101780@gm.pu.edu.tw>
2022-10-30 19:48:13 +0800

    興增前序

---
+++ infixtoprefix.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MaxNum 20 //定義堆疊大小
+char Stack[MaxNum] = {'0'}; //以陣列Stack當作堆疊
+int InfixToPostfix(char []); //宣告中序轉後序
+int Priority(char); //宣告決定運算子優先順序
+int j=0,Top = -1; 
+int length=sizeof(Stack) / sizeof(char);
+int main(void) 
+{ 
+char itemset[20];
+printf("===============程式描述================\n");
+printf("= 程式名稱：ch3-3.5.c =\n");
+printf("= 程式目的：中序轉前序 =\n"); 
+printf("=======================================\n"); 
+printf("=================輸入==================\n");
+printf("請輸入中序運算式："); 
+scanf("%s", itemset); 
+printf("=================輸出==================\n"); 
+InfixToPostfix(itemset); 
+return (0); 
+}
+//中序轉後序之副程式
+int InfixToPostfix(char Stack[]) 
+{ 
+
+char Stack1[100]= {'0'};
+char Stack2[100]= {'0'};
+int i,k;
+i=k=strlen(Stack)-1;
+/*for(int z=k;z>=0;z--){
+    printf("%c",Stack[z]);
+}*/
+
+/*printf("\n");
+printf("長度:%d\n",i);*/
+char op; 
+printf("輸出前序式結果："); 
+while(1) 
+{ 
+/*printf("\n");
+printf("i:%d\n",i);*/
+if(i==-1){ 
+/*
+while(Top >=0) 
+{ 
+printf("%c", Stack[Top]); 
+
+Top--; 
+} 
+printf("\n"); 
+system("pause"); //使程式暫停在執行畫面
+*/
+while(Top >=0) 
+{ 
+Stack1[j]=Stack2[Top];
+Top--; 
+} 
+for(int m=k;m>=0;m--){
+    printf("%c",Stack1[m]);
+}
+printf("\n"); 
+return (0);}
+
+op = Stack[i]; 
+/*printf("op:%c\n",op);*/
+switch(op) 
+{ 
+/*
+case '\0': 
+while(Top >=0) 
+{ 
+printf("%c", Stack[Top]); 
+Top--; 
+} 
+printf("\n"); 
+system("pause"); //使程式暫停在執行畫面
+return (0);
+*/
+
+// 運算子堆疊
+case ')': 
+if(Top < length)
+{ 
+Top++; 
+Stack2[Top] = op; 
+
+} 
+break; 
+case '+': case '-': case '*': case '/':
+while(Priority(Stack2[Top]) >= Priority(op)) 
+{ 
+//printf("%c", Stack[Top]); 
+Stack1[j]=Stack2[Top];
+//printf("%c", Stack1[j]);
+Top--; 
+} 
+// 存入堆疊
+if(Top < length) 
+{ 
+Top++; 
+Stack2[Top] = op; 
+} 
+break;
+// 遇 ) 輸出至 ( 
+case '(': 
+while(Stack2[Top] != ')') 
+{ 
+//printf("%c", Stack[Top]); 
+Stack1[j]=Stack2[Top];
+//printf("%c", Stack1[j]);
+Top--; 
+} 
+Top--; // 不輸出( 
+break; 
+// 運算元直接輸出
+default: 
+//printf("%c", op); 
+Stack1[j]=op;
+//printf("%c", Stack1[j]);
+break; 
+
+} 
+if(Stack1[j]!='0'){j++;}
+i--; 
+}
+} 
+
+int Priority(char op) 
+{ 
+int p; 
+switch(op) 
+{ 
+case '+': case '-': 
+p = 1; 
+break; 
+case '*': case '/': 
+p = 2; 
+break; 
+default: 
+p = 0; 
+break; 
+} 
+return p;
+} 
